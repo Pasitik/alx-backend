6bfe945bc70c20f1bfe270329890faef39e64d83
pasitik <paasitik@gmail.com>
2024-01-27 23:57:04 +0000

    0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,7 +6,7 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ """
     if page < 1 or page_size < 1:
-        raise "page/page size cannot be less than 1"
+        raise ValueError("page/page size cannot be less than 1")
     end = page * page_size
     start = end - page_size
     return (start, end)
