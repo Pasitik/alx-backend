7c728f01574530b20f6fbfcf9f5d9fdfe611a46d
pasitik <paasitik@gmail.com>
2024-01-31 23:14:26 +0000

    cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,6 +9,12 @@ class BasicCache(BaseCaching):
     """
         Defines a class for caching information in key-value pairs
     """
+    def __init__(self):
+        """
+        Initialize the class using the parent class __init__ method
+        """
+        BaseCaching.__init__(self)
+
     def put(self, key, item):
         if key is None or item is None:
             pass
