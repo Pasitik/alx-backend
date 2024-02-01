d99ac419858d53062da0a7610cb02e07944cc53c
pasitik <paasitik@gmail.com>
2024-02-01 01:53:04 +0000

    cache

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -10,10 +10,12 @@ class LIFOCache(BaseCaching):
         LIFO
     """
     def __init__(self):
+        """class initialization"""
         super().__init__()
         self.keys = []
 
     def put(self, key, item):
+        """updates cache"""
         if key is None or item is None:
             pass
         else:
@@ -29,6 +31,7 @@ class LIFOCache(BaseCaching):
             self.keys.append(key)
 
     def get(self, key):
+        """retrieves item from cache"""
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
