62c2857f5955fa4524283b6beb1b04802964aeb0
pasitik <paasitik@gmail.com>
2024-02-01 01:41:11 +0000

    cache

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -11,7 +11,7 @@ class LRUCache(BaseCaching):
     """
     def __init__(self):
         super().__init__()
-        self.keys = []
+        self.keys_list = []
 
     def put(self, key, item):
         if key is None or item is None:
@@ -19,19 +19,19 @@ class LRUCache(BaseCaching):
         else:
             if len(self.cache_data.keys()) >= BaseCaching.MAX_ITEMS \
                     and key not in self.cache_data:
-                del self.cache_data[self.keys[0]]
-                print("DISCARD: {}".format(self.keys[0]))
-                del self.keys[0]
-            if key in self.keys:
-                del self.keys[self.keys.index(key)]
+                del self.cache_data[self.keys_list[0]]
+                print("DISCARD: {}".format(self.keys_list[0]))
+                del self.keys_list[0]
+            if key in self.keys_list:
+                del self.keys_list[self.keys_list.index(key)]
 
             self.cache_data[key] = item
-            self.keys.append(key)
+            self.keys_list.append(key)
 
     def get(self, key):
         if key is None or key not in self.cache_data.keys():
             return None
-        if key in self.keys:
-            del self.keys[self.keys.index(key)]
-        self.keys.append(key)
+        if key in self.keys_list:
+            del self.keys_list[self.keys_list.index(key)]
+        self.keys_list.append(key)
         return self.cache_data[key]
