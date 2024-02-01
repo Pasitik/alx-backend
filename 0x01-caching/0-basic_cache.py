f84825d5d45f52dd0ab5a628e60a608c8528f4a5
pasitik <paasitik@gmail.com>
2024-02-01 01:29:16 +0000

    cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,14 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ BaseCaching module
 """
-
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """
-        Defines a class for caching information in key-value pairs
+    Defines a class for caching information in key-value pairs
+    Methods:
+        put(key, item) - store a key-value pair
+        get(key) - retrieve the value associated with a key
     """
+
     def __init__(self):
         """
         Initialize the class using the parent class __init__ method
@@ -16,13 +19,22 @@ class BasicCache(BaseCaching):
         BaseCaching.__init__(self)
 
     def put(self, key, item):
+        """
+        Store a key-value pair
+        Args:
+            Key
+            Item
+        """
         if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
-        if key is None or key not in self.cache_data.keys():
-            return None
-        else:
+        """
+        Return value linked to key.
+        If key is None or doesn't exist, return None
+        """
+        if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
+        return None
